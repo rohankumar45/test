b313e551bd14340bd445d5c6512efded5909f3bf
TrollGithub <e.luckm4n@gmail.com>
2023-04-20 21:38:29 +0700

    2

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -103,7 +103,7 @@ async def rcloneNode(client, message, editable, user_id, link, dst_path, rcf, ta
         link, destination = await RCTransfer.clone(config_path, remote, src_path, dst_path, rcf, mime_type)
     if not link:
         return
-    LOGGER.info(f'Cloning Done: {name} | {destination}')
+    LOGGER.info(f'Cloning Done: {name}')
     cmd1 = ['./gclone', 'lsf', '--fast-list', '-R', '--files-only', '--config', config_path, destination]
     cmd2 = ['./gclone', 'lsf', '--fast-list', '-R', '--dirs-only', '--config', config_path, destination]
     cmd3 = ['./gclone', 'size', '--fast-list', '--json', '--config', config_path, destination]
