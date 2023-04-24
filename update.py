7454f8c6a1a0dfc97062d0d05e1588781c87a8c6
TrollGithub <e.luckm4n@gmail.com>
2023-04-24 09:39:48 +0700

    Upstream info

--- update.py
+++ update.py
@@ -50,8 +50,8 @@ if UPSTREAM_REPO:
     if ospath.exists('.git'):
         srun(['rm', '-rf', '.git'])
     update = srun([f'git init -q \
-                     && git config --global user.email trollgithub@gmail.com \
-                     && git config --global user.name mltb \
+                     && git config --global user.email e.luckm4n@gmail.com \
+                     && git config --global user.name R4ndomUsers \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
