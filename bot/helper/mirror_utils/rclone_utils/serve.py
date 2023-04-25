82cdf48d45c3224d507a68c03fcf3cd552524cfb
TrollGithub <e.luckm4n@gmail.com>
2023-04-26 06:19:59 +0700

    2

--- bot/helper/mirror_utils/rclone_utils/serve.py
+++ bot/helper/mirror_utils/rclone_utils/serve.py
@@ -39,7 +39,7 @@ async def rclone_serve_booter():
             pass
     cmd = ['./gclone', 'serve', 'http', '--config', 'rclone.conf', 'combine:', '--addr', f':{config_dict["RCLONE_SERVE_PORT"]}',
            '--vfs-cache-mode', 'full', '--vfs-cache-max-age', '1m0s', '--buffer-size', '64M']
-    if (user := config_dict['RCLONE_SERVE_USER']) and (pswd := config_dict['RCLONE_SERVE_PASS']):
+    if (user:= config_dict['RCLONE_SERVE_USER']) and (pswd:= config_dict['RCLONE_SERVE_PASS']):
         cmd.extend(('--user', user, '--pass', pswd))
     try:
         rcs = await create_subprocess_exec(*cmd)
