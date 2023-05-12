8c48cdf179d0db1202e851156157dd4106045876
R4ndomUsers <e.luckm4n@gmail.com>
2023-05-12 13:15:40 +0700

    2

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -5,24 +5,24 @@ from bot import user_data, OWNER_ID
 
 class CustomFilters:
 
-    async def owner_filter(self, client, update):
-        user = update.from_user or update.sender_chat
+    async def owner_filter(self, client, message):
+        user = message.from_user or message.sender_chat
         uid = user.id
         return uid == OWNER_ID
 
     owner = create(owner_filter)
 
-    async def authorized_user(self, client, update):
-        user = update.from_user or update.sender_chat
+    async def authorized_user(self, client, message):
+        user = message.from_user or message.sender_chat
         uid = user.id
-        chat_id = update.chat.id
+        chat_id = message.chat.id
         return bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or
                     (chat_id in user_data and user_data[chat_id].get('is_auth', False)))
 
     authorized = create(authorized_user)
 
-    async def sudo_user(self, client, update):
-        user = update.from_user or update.sender_chat
+    async def sudo_user(self, client, message):
+        user = message.from_user or message.sender_chat
         uid = user.id
         return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
 
