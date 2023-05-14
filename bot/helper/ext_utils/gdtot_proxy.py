c5f0ac2c2c4136d599d8e2fc05701903d9b332dc
R4ndomUsers <e.luckm4n@gmail.com>
2023-05-14 08:39:01 +0700

    Fix gdtot search

--- bot/helper/ext_utils/gdtot_proxy.py
+++ bot/helper/ext_utils/gdtot_proxy.py
@@ -6,9 +6,9 @@ from urllib.parse import urlparse, quote_plus
 
 async def search_gdtot(query):
     async with ClientSession() as session:
-        async with session.get(f'https://gdbot.xyz/search?q={quote_plus(query)}') as r:
+        async with session.get(f'https://gdtot.pro/search?q={quote_plus(query)}') as r:
             soup = BeautifulSoup(await r.read(), 'html.parser')
-            links = soup.select("a[href*='https://gdbot.xyz/file']")
+            links = soup.select("a[href*='https://gdtot.pro/file']")
             info = [x.string for x in soup.find_all('span', string=re_compile(r'Size*'))]
             titles = [x.string for x in soup.find_all('a')[5:]]
             text, result = '', []
