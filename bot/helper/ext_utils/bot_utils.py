78592325007716bc2eb5b6446166db2bc209a904
R4ndomUsers <e.luckm4n@gmail.com>
2023-05-15 10:44:27 +0700

    Fix for get link

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -393,9 +393,9 @@ async def update_user_ldata(id_: int, key: str, value):
 async def get_link(message: Message):
     link = ''
     reply_to = message.reply_to_message
-    pattern = r'[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+    pattern = r'[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(magnet:\?xt=urn:(btih|btmh):[-a-zA-Z0-9@:%_\+.~#?&//=]*\s*)'
     if match:= re_match(pattern, message.text.strip()):
-        link = match.group(1)
+        link = match.group()
     if reply_to:
         if (media:= is_media(reply_to)):
             link = f'Source is media/file: {media.mime_type}' if not reply_to.photo else 'Source is image/photo'
