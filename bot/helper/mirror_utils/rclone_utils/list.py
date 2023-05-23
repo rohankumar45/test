a3fa191797ae52849f38556e82f1b11e5c72cc57
R4ndomUsers <e.luckm4n@gmail.com>
2023-05-23 11:53:03 +0700

    2

--- bot/helper/mirror_utils/rclone_utils/list.py
+++ bot/helper/mirror_utils/rclone_utils/list.py
@@ -199,7 +199,7 @@ async def path_updates(_, query: CallbackQuery, obj: RcloneList):
     await query.answer()
     data = query.data.split()
     LOGGER.info(data)
-    LOGGER.info(obj.remote)
+    LOGGER.info(obj.path)
     if data[1] == 'cancel':
         obj.remote = 'Task has been cancelled!'
         obj.path = ''
