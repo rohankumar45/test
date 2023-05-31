71a7192b129681185688364fe34fa446c95d1eb5
R4ndomUsers <e.luckm4n@gmail.com>
2023-05-31 16:00:50 +0700

    2

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -4,25 +4,25 @@ from bot import user_data, OWNER_ID
 
 
 class CustomFilters:
-    async def owner_filter(self, client, message):
+    async def owner_filter(self, _, message):
         user = message.from_user or message.sender_chat
         uid = user.id
         return uid == OWNER_ID
 
     owner = create(owner_filter)
 
-    async def authorized_user(self, client, message):
+    async def authorized_user(self, _, message):
         user = message.from_user or message.sender_chat
         uid = user.id
         chat_id = message.chat.id
-        return bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or
-                    (chat_id in user_data and user_data[chat_id].get('is_auth', False)))
+        user_dict = user_data.get(uid, {})
+        return bool(uid == OWNER_ID or (user_dict.get('is_auth') or user_dict.get('is_sudo')) or user_data.get(chat_id, {}).get('is_auth'))
 
     authorized = create(authorized_user)
 
-    async def sudo_user(self, client, message):
+    async def sudo_user(self, _, message):
         user = message.from_user or message.sender_chat
         uid = user.id
-        return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
+        return bool(uid == OWNER_ID or user_data.get(uid, {}).get('is_sudo'))
 
     sudo = create(sudo_user)
