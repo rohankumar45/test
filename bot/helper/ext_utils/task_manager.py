568eeedec86d136962956fd0022998eb966578c6
R4ndomUsers <e.luckm4n@gmail.com>
2023-06-04 12:54:37 +0700

    2

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -28,9 +28,7 @@ async def stop_duplicate_check(name: str, listener, mega_type='folder'):
             except:
                 name = None
         if name:
-            is_file = await aiopath.isfile(f'{listener.dir}/{name}')
             if not listener.newname and (await aiopath.isfile(f'{listener.dir}/{name}') or mega_type == 'file'):
-                LOGGER.info('================================================')
                 name = presuf_remname_name(listener.user_dict, name)
             count, file = await sync_to_async(GoogleDriveHelper().drive_list, name, stopDup=True)
             if count:
